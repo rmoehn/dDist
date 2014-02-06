726a76e7083194a4d5d1100642893d7e81148d6a
Richard Möhn <richard.moehn@fu-berlin.de>
2014-02-06 08:29:34 +0100

    Clean up NodeRunner for presentation

--- ex07/src/main/java/ddist/NodeRunner.java
+++ ex07/src/main/java/ddist/NodeRunner.java
@@ -1,72 +1,74 @@
 package ddist;
 
 import java.rmi.RemoteException;
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.*;
 import ddist.IChordNode;
 
-
 public class NodeRunner {
-
-    private static final String CORDNODE = "cordnode";
+    private static final String CHORDNODE = "chordnode";
 
     public static void main(final String[] args) throws Exception {
-
-        int ringBitSize;
-        String localHostname = "";
-        int localPort;
-        String firstNodeHostname = "";
-        int firstNodePort;
-
-
-        if (args.length == 3) {
-            ringBitSize = Integer.parseInt(args[0]);
-            ChordNode node = new ChordNode();
-            node.newNetwork(ringBitSize);
-            createServer(node, args[1], Integer.parseInt(args[2]));
+        // Check arguments
+        if ((args.length != 2) && (args.length != 4)) {
+            System.err.println("Wrong number of arguments.");
+            System.exit(1);
         }
 
-        if (args.length == 5) {
-            ringBitSize = Integer.parseInt(args[0]);
-            firstNodeHostname = args[3];
-            firstNodePort = Integer.parseInt(args[4]);
+        // Create node for this process
+        int ringBitSize     = Integer.parseInt(args[0]);
+        ChordNode localNode = new ChordNode();
 
-            IChordNode remoteNode
-                = (IChordNode) Naming.lookup("//" + firstNodeHostname + ":"
-                                             + firstNodePort + "/" + CORDNODE);
-            System.out.printf("Contactet node whith ID %d%n", remoteNode.getID());
-
-            ChordNode node = new ChordNode();
-            node.join(remoteNode);
-            createServer(node, args[1], Integer.parseInt(args[2]));
+        // Start as first node in ring
+        if (args.length == 2) {
+            localNode.newNetwork(ringBitSize);
         }
+        // Join network through node specified by args[2]:args[3]
+        else if (args.length == 4) {
+            IChordNode remoteNode = (IChordNode) Naming.lookup(
+                String.format("//%s:%s/%s", args[2], args[3], CHORDNODE) );
+            System.out.println(
+                "Contacted node whith ID " + remoteNode.getID() );
+
+            localNode.join(remoteNode);
+        }
+
+        // Make local node accessible on localhost:args[1]
+        createServer( localNode, Integer.parseInt(args[1]) );
     }
 
-    private static void createServer(final IChordNode node, String localHostname, int localPort) throws Exception {
+    private static void createServer(final IChordNode node, int localPort)
+            throws Exception {
+        // Put node in a new registry
         LocateRegistry.createRegistry(localPort);
-        Naming.rebind("//localhost:" + localPort + "/" + CORDNODE, node);
+        Naming.rebind(
+            String.format("//localhost:%d/%s", localPort, CHORDNODE),
+            node
+        );
+        System.out.printf(
+            "New server started on localhost:%d with ID %d%n",
+            localPort, node.getID()
+        );
 
-        System.out.printf("New server started on %s:%d With ID %s%n",
-                          localHostname, localPort, node.getID());
-
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-                public void run() {
-                    try {
-                        node.leave();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
+        // Make nodes leave cleanly if the JVM is terminated
+        Runtime.getRuntime().addShutdownHook( new Thread() {
+            public void run() {
+                try {
+                    node.leave();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            });
+            }
+        } );
 
 
+        // Continuously print network information, keeping server alive
         while (true) {
             System.out.println("#########");
-            System.out.println(node.ringToString());
+            System.out.println( node.ringToString() );
             System.out.println("#########");
             Thread.sleep(3000);
-        } // Keep the server alive.
+        }
     }
 }
