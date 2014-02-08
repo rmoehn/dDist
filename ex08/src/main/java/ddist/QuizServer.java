eae345bfd2a33bffe4218891691abf1801dd31c6
Richard Möhn <richard.moehn@fu-berlin.de>
2014-02-08 13:00:12 +0100

    Add undebugged server for ex 08

--- ex08/src/main/java/ddist/QuizServer.java
+++ ex08/src/main/java/ddist/QuizServer.java
@@ -5,30 +5,52 @@ public class QuizServer {
     private final BlockingQueue<QA> _qaQueue = new LinkedBlockingQueue<>();
 
     /**
-     * @param port the port on which this server should accept new clients
+     * @param port the port on which this server should accept a client
      */
     public QuizServer(int port) {
         _port = port;
     }
 
     public void run() {
-        // Create socket and wait for clients
+        // Create input stream for the command line
+        BufferedReader stdin = new BufferedReader(
+                                   new InputStreamReader(
+                                       System.in
+                                   )
+                               );
+
+        // Create socket and wait for a client
+        ServerSocket servSock = new ServerSocket(port);
+        Socket sock           = servSock.accept();
 
         // Create input and output streams on the socket
+        ObjectInputStream objIn
+            = new ObjectInputStream( sock.getInputStream() );
+        ObjectOutputStream objOut
+            = new ObjectOutputStream( sock.getOutputStream() );
 
         // Start thread for receiving QAs
-        //      Wait for incoming question
-        //      Add it to the queue
-        //      Repeat
+        Thread inManager = new Thread() {
+            public void run() {
+                // Add incoming questions to the queue
+                while ( QA qa = (QA) objIn.readObject() ) {
+                    _qaQueue.put(qa);
+                }
+            }
+        };
 
-        // Create input and output streams for the command line
+        // Wait for QAs in the queue
+        while ( QA qa = _qaQueue.take() ) {
+            // Print question
+            System.out.println("Q: " + qa.getQuestion());
+            System.out.println("A? ");
 
-        // Wait for items in the queue
-        //      Remove QA
-        //      Print question
-        //      Wait for input
-        //      Write input to QA
-        //      Send QA back to client
-        //      Repeat
+            // Read answer
+            String answer = stdin.readLine();
+
+            // Put it into the QA and send back
+            qa.setAnswer(answer);
+            objOut.writeObject(qa);
+        }
     }
 }
