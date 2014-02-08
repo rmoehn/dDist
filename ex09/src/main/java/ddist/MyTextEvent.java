2194cca708b0b334aff5b548f4bf322d20cac0ea
Richard Möhn <richard.moehn@fu-berlin.de>
2014-02-08 10:35:16 +0100

    Add adjusted provided files for DistributedTextEditor

---
+++ ex09/src/main/java/ddist/MyTextEvent.java
@@ -0,0 +1,14 @@
+package ddist;
+
+/**
+ *
+ * @author Jesper Buus Nielsen
+ *
+ */
+public class MyTextEvent {
+	MyTextEvent(int offset) {
+		this.offset = offset;
+	}
+	private int offset;
+	int getOffset() { return offset; }
+}
---
+++ ex09/src/main/java/ddist/TextInsertEvent.java
@@ -0,0 +1,18 @@
+package ddist;
+
+/**
+ *
+ * @author Jesper Buus Nielsen
+ *
+ */
+public class TextInsertEvent extends MyTextEvent {
+
+	private String text;
+
+	public TextInsertEvent(int offset, String text) {
+		super(offset);
+		this.text = text;
+	}
+	public String getText() { return text; }
+}
+
