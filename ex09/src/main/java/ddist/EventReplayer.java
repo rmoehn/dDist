8a436725c568e1ad8354145bfaa325ecab88206a
Mathias Dannesbo <neic@neic.dk>
2014-02-09 21:50:24 +0100

    Proper indentation and linebreaks

--- ex09/src/main/java/ddist/EventReplayer.java
+++ ex09/src/main/java/ddist/EventReplayer.java
@@ -28,59 +28,64 @@ public class EventReplayer implements Runnable {
      * @param frame the overall frame of the program (might be done better)
      */
     public EventReplayer(BlockingQueue<MyTextEvent> eventQueue,
-            JTextArea area, JFrame frame) {
-	this.eventQueue = eventQueue;
-	this.area = area;
-    this.frame = frame;
+                         JTextArea area, JFrame frame) {
+        this.eventQueue = eventQueue;
+        this.area = area;
+        this.frame = frame;
     }
 
     public void run() {
-	boolean wasInterrupted = false;
-	while (!wasInterrupted) {
-	    try {
-		MyTextEvent mte = eventQueue.take();
-		if (mte instanceof TextInsertEvent) {
-		    final TextInsertEvent tie = (TextInsertEvent)mte;
-		    EventQueue.invokeLater(new Runnable() {
-			    public void run() {
-				try {
-				    area.insert(tie.getText(), tie.getOffset());
-				} catch (Exception e) {
-				    System.err.println(e);
-				    /* We catch all axceptions, as an uncaught exception would make the
-				     * EDT unwind, which is now healthy.
-				     */
-				}
-			    }
-			});
-		} else if (mte instanceof TextRemoveEvent) {
-		    final TextRemoveEvent tre = (TextRemoveEvent)mte;
-		    EventQueue.invokeLater(new Runnable() {
-			    public void run() {
-				try {
-				    area.replaceRange(null, tre.getOffset(), tre.getOffset()+tre.getLength());
-				} catch (Exception e) {
-				    System.err.println(e);
-				    /* We catch all axceptions, as an uncaught exception would make the
-				     * EDT unwind, which is now healthy.
-				     */
-				}
-			    }
-			});
-		}
-        else if (mte instanceof DisconnectEvent) {
-            JOptionPane.showMessageDialog(frame, "Disconnected.");
-            frame.setTitle("Disconnected");
-            area.setText("");
+        boolean wasInterrupted = false;
+        while (!wasInterrupted) {
+            try {
+                MyTextEvent mte = eventQueue.take();
+                if (mte instanceof TextInsertEvent) {
+                    final TextInsertEvent tie = (TextInsertEvent)mte;
+                    EventQueue.invokeLater(new Runnable() {
+                            public void run() {
+                                try {
+                                    area.insert(tie.getText(), tie.getOffset());
+                                } catch (Exception e) {
+                                    System.err.println(e);
+                                    /* We catch all axceptions, as an uncaught
+                                     * exception would make the EDT unwind,
+                                     * which is now healthy.
+                                     */
+                                }
+                            }
+                        });
+                } else if (mte instanceof TextRemoveEvent) {
+                    final TextRemoveEvent tre = (TextRemoveEvent)mte;
+                    EventQueue.invokeLater(new Runnable() {
+                            public void run() {
+                                try {
+                                    area.replaceRange(null, tre.getOffset(),
+                                                      tre.getOffset() + 
+                                                      tre.getLength());
+                                } catch (Exception e) {
+                                    System.err.println(e);
+                                    /* We catch all axceptions, as an uncaught
+                                     * exception would make the EDT unwind,
+                                     * which is now healthy.
+                                     */
+                                }
+                            }
+                        });
+                }
+                else if (mte instanceof DisconnectEvent) {
+                    JOptionPane.showMessageDialog(frame, "Disconnected.");
+                    frame.setTitle("Disconnected");
+                    area.setText("");
+                }
+                else {
+                    System.err.println("Illegal event received.");
+                    System.exit(1);
+                }
+            } catch (Exception _) {
+                wasInterrupted = true;
+            }
         }
-        else {
-            System.err.println("Illegal event received.");
-            System.exit(1);
-        }
-	    } catch (Exception _) {
-		wasInterrupted = true;
-	    }
-	}
-	System.out.println("I'm the thread running the EventReplayer, now I die!");
+        System.out.println(
+            "I'm the thread running the EventReplayer, now I die!");
     }
 }
