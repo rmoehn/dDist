ea05486b13801a679c1f227ffefe778e93a06c91
Richard Möhn <richard.moehn@fu-berlin.de>
2014-03-09 09:42:32 +0100

    Add Client for managing client runtime (analogous to Server)

---
+++ ex09/src/main/java/ddist/Client.java
@@ -0,0 +1,58 @@
+package ddist;
+
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Client {
+    private final Socket _connectSocket;
+    private final int _listenPort;
+    private final BlockingQueue<Event> _inQueue;
+    private final BlockingQueue<Event> _toDisplayer;
+    private final BlockingQueue<Event> _outQueue;
+    private final ClientEventDistributor _eventDistributor;
+
+    public Client(Socket connectSocket, int listenPort) {
+        _listenPort     = listenPort;
+        _connectSocket  = connectSocket;
+        _inQueue        = new LinkedBlockingQueue<>();
+        _toDisplayer    = new LinkedBlockingQueue<>();
+        _outQueue       = new LinkedBlockingQueue<>();
+        BlockingQueue<Event> outQueue = new LinkedBlockingQueue<Event>();
+        _eventDistributor = new ClientEventDistributor(
+                                this,
+                                _inQueue,
+                                outQueue,
+                                _toDisplayer
+                            );
+    }
+
+    public void start() {
+        // Start thread containing the Jupiter client/server
+        Thread eventDistributorThread = new Thread(_eventDistributor);
+        eventDistributorThread.start();
+
+        // Start thread for adding incoming events to the inqueue
+        EventReceiver rec
+            = new EventReceiver(_connectSocket, _inQueue, _outQueue);
+        Thread receiverThread = new Thread(rec);
+        receiverThread.start();
+
+        // Start thread for taking outgoing events from the outqueue
+        EventSender sender = new EventSender(_connectSocket, _outQueue);
+        Thread senderThread = new Thread(sender);
+        senderThread.start();
+    }
+
+    public BlockingQueue<Event> getInQueue() {
+        return _inQueue;
+    }
+
+    public BlockingQueue<Event> getQueueToDisplayer() {
+        return _toDisplayer;
+    }
+    
+    public int getListenPort() {
+        return _listenPort;
+    }
+}
