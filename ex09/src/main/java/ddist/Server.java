ed8b2537e8ec5d884cf1c2e4cd2dd11dc5d627d4
Richard Möhn <richard.moehn@fu-berlin.de>
2014-03-09 09:44:25 +0100

    Server: Adapt to other changes concerning server switiching

--- ex09/src/main/java/ddist/Server.java
+++ ex09/src/main/java/ddist/Server.java
@@ -1,24 +1,37 @@
 package ddist;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Semaphore;
 
 public class Server {
-
     private int _port;
+    private InetAddress _address;
     ServerEventDistributor _eventDistributor;
+    private Semaphore _readyForAccepting = new Semaphore(0);
 
-    public Server(int port) {
-        _port = port;
-        _eventDistributor = new ServerEventDistributor();
+    public Server(int port, String initialText, int oldClientsCount) {
+        _port             = port;
+        _eventDistributor = new ServerEventDistributor(
+                                initialText, oldClientsCount);
     }
 
-    public void start(Semaphore mayConnect) {
+    public Server(int port) {
+        this(port, "", 0);
+    }
+
+    public void start() {
         listenForConnection();
         startEventDistributor();
-        mayConnect.release();
+
+        try {
+            _readyForAccepting.acquire();
+        }
+        catch (InterruptedException e) {
+            throw new AssertionError();
+        }
     }
 
     private void listenForConnection() {
@@ -27,7 +40,10 @@ public class Server {
                 public void run() {
                     try {
                         @SuppressWarnings("resource")
-                            ServerSocket servSock = new ServerSocket(_port);
+                        ServerSocket servSock = new ServerSocket(_port);
+                        _address = servSock.getInetAddress();
+                        _readyForAccepting.release();
+
                         while (true) {
                             // Wait for an incoming connection
                             Socket socket = null;
@@ -42,6 +58,14 @@ public class Server {
                     }
                 }
             } ).start();
+    }
+
+    public InetAddress getListenAddress() {
+        return _address;
+    }
+
+    public int getListenPort() {
+        return _port;
     }
 
     private void startEventDistributor() {
